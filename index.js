9acc96948ed7b35a3ba10a4650cc32612231c953
Kaio Guilherme Bauer <kaiobbauer@gmail.com>
2024-05-07 15:26:26 -0300

    feat: adicionar conexao com banco de dados e rota de teste

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const express = require('express')
 const app = express()
+const db = require('./db/connection')
 
 const PORT = 3000
 
 app.listen(PORT, function(){
     console.log(`O express etá rodando na porta ${PORT}`)
+})
+
+//db connection
+
+db
+    .authenticate()
+    .then(() =>{
+        console.log('Conectado ao DB')
+    })
+    .catch(err => {
+        console.log("Ocorreu um erro ao iniciar o DB", err)
+    })
+
+
+
+
+//routes
+app.get('/', (req, res) => {
+    res.send('Esta fuuncionando')
 })
